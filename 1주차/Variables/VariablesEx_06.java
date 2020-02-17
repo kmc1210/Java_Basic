ac4f5b415f3fdbb2d5d9ec5f030e8c5955a7c29b
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-02-17 10:58:24 +0900

    Create VariablesEx_06.java

---
+++ 1주차/Variables/VariablesEx_06.java
@@ -0,0 +1,24 @@
+package java01_variables;
+
+public class VariablesEx_06 {
+	public static void main(String[] args) {
+		
+		// 참조형 데이터타입 ? - 객체지향 프로그래밍?
+		
+		// String, 문자열 
+		// 단일문자(char) 들의 나열
+		// 문장을 표현하는 데이터타입
+		
+		String fruit; // 문자열 변수 선언
+		fruit = "Apple"; //  문자열 상수 "Apple"을 저장(대입)
+		System.out.println(fruit); //문자열 변수 출력
+		
+		
+		
+		fruit = "Banana";
+		System.out.println(fruit);
+		
+		fruit = "Grape";
+		System.out.println("과일명 : " + fruit);
+	}
+}
