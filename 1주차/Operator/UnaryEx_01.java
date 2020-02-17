65b3ac20f06a9ed68178053a6220c560b33065f4
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-02-17 16:29:48 +0900

    Create UnaryEx_01.java

---
+++ 1주차/Operator/UnaryEx_01.java
@@ -0,0 +1,30 @@
+package java02_operator;
+
+
+public class UnaryEx_01 {
+	
+// java02_operator.UnaryEx_01.main
+	public static void main(String[] args) {
+		
+		//단항연산자 - 부호
+		// + -
+		
+		int num = -7; // +7;
+		
+		System.out.println( -num);
+//		System.out.println( -(-7));
+//		System.out.println( +7);
+		
+		
+		
+		System.out.println ( +num);
+//		System.out.println ( +(-7));
+//		System.out.println ( -7);
+		
+		// ! : 논리 부정 연산자
+		// (type) : 형변환 연산자
+		// . : 참조 연산자
+		
+		
+	}
+}
