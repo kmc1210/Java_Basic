effa84124aad57a53882f763d3d89da7bff79b6d
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-02-17 10:57:28 +0900

    Create VariablesEx_03.java

---
+++ 1주차/Variables/VariablesEx_03.java
@@ -0,0 +1,21 @@
+package java01_variables;
+
+public class Variables_03 {
+
+	public static void main(String[] args) {
+		
+		//변수의 선언과 동시에 초기화(Initialize)
+		int num = 123; //변수 선언부터 초기화 사이의 틈을 없애기 위해 주로 사용?
+		System.out.println(num);
+
+		int age; //변수 선언
+		age  = 11;
+		
+		System.out.println(age); //쓰레기값을 출력하려고 해서 에러가 발생한다. (변수를 초기화 하지 않은 경우)
+		// 해결 : age 변수를 초기화(대입) 해주고 사용(활용)해야 한다.
+		
+		
+		
+	}
+
+}
