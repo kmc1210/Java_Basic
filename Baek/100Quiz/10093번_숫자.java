a7653342c5976a7a28b3b8680d9ac1c484743f4c
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-03-02 18:09:17 +0900

    Create 10093번_숫자.java

---
+++ Baek/100Quiz/10093번_숫자.java
@@ -0,0 +1,29 @@
+public class Baek_10093 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("값을 입력하시오 : ");
+		
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		
+		if(B>A) {
+		System.out.println(B-A);
+		for(long i=1; i<=(B-A)-1; i++) {
+			System.out.print(A+i+" ");
+			}
+		}//B가A보다 클 경우
+		
+		else if(A>B) {
+		System.out.println(A-B);
+		for(long i=1; i<=(A-B)-1; i++) {
+			System.out.print(B+i+" ");
+			}
+		}
+		
+		else if(A==B) {
+		System.out.println(A-B);
+		}
+	}
+}
+//런타임에러 뜸 왜그런거야 ㅠㅠㅠ
