8499547f6d0a6bf0fcbfd34385d1cabe05b9748a
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-03-02 16:57:15 +0900

    Create If_01.java

---
+++ 2주차/Control/condition/If_01.java
@@ -0,0 +1,21 @@
+package java04_control.condition;
+
+public class If_01 {
+	public static void main(String[] args) {
+		
+		//조건문 - if
+		
+//		if( ) {
+			// 조건식이 참일 경우 수행하는 코드 영역
+//		}
+		// 조건식은 연산의 결과가 true/false(boolean)으로 판별가능한 연산식
+		
+		if(true ) {//if(false) {
+			System.out.println("출력될까??");
+		}	
+		// 
+			System.out.println("여기는 if문 바깥쪽 코드");
+			
+	
+	}
+}
