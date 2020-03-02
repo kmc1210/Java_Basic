7eea75b50e607fe206a00e2f95c6d5f1c8caab1b
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-03-02 17:13:07 +0900

    Create 3046번_R2.java

---
+++ Baek/100Quiz/3046번_R2.java
@@ -0,0 +1,13 @@
+package baek;
+
+import java.util.*;
+
+public class Baek_3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		int R2 = (2*S)-R1;
+		System.out.println(R2);
+	}
+}
