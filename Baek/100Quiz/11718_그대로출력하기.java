6fd32d2173b8f82a2d4d5e870186dce317a6503f
kmc1210 <57215151+kmc1210@users.noreply.github.com>
2020-03-03 16:13:59 +0900

    Create 11718_그대로출력하기.java

---
+++ Baek/100Quiz/11718_그대로출력하기.java
@@ -0,0 +1,13 @@
+package baek;
+
+import java.util.*;
+
+public class Baek_11718 {
+	public static void main(String[] args) {
+		Scanner sc  =  new Scanner(System.in);
+		while(sc.hasNextLine()) {
+			String input = sc.nextLine();
+			System.out.println(input);
+		}
+	}
+}
